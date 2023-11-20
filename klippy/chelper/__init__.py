ab980bdfab0272e4c29825bebf4eecc0fe683caf
klagam <151570753+klagam@users.noreply.github.com>
2023-11-20 19:47:56 +0000

    Update __init__.py
    Changing "__init__.py" to match "__init__.py" in: https://github.com/PlybotPrinter/klipper/blob/341ca7f0e34b7e77554ccc7cc8491b8ef724bf07/klippy/chelper/__init__.py

--- klippy/chelper/__init__.py
+++ klippy/chelper/__init__.py
@@ -20,7 +20,7 @@ SOURCE_FILES = [
     'pyhelper.c', 'serialqueue.c', 'stepcompress.c', 'itersolve.c', 'trapq.c',
     'pollreactor.c', 'msgblock.c', 'trdispatch.c',
     'kin_cartesian.c', 'kin_corexy.c', 'kin_corexz.c', 'kin_delta.c',
-    'kin_deltesian.c', 'kin_polar.c', 'kin_rotary_delta.c', 'kin_winch.c',
+    'kin_deltesian.c', 'kin_polar.c', 'kin_rotary_delta.c', 'kin_winch.c', 'kin_fivebarelbow.c',
     'kin_extruder.c', 'kin_shaper.c', 'kin_idex.c',
 ]
 DEST_LIB = "c_helper.so"
@@ -138,6 +138,14 @@ defs_kin_winch = """
         , double anchor_y, double anchor_z);
 """
 
+defs_kin_fivebarelbow = """
+    struct stepper_kinematics *
+    fivebarelbow_stepper_alloc(char arm,
+                           double inner_arm_length,
+                           double outer_arm_length,
+                           double inner_arms_distance);
+"""
+
 defs_kin_extruder = """
     struct stepper_kinematics *extruder_stepper_alloc(void);
     void extruder_set_pressure_advance(struct stepper_kinematics *sk
@@ -219,7 +227,7 @@ defs_all = [
     defs_pyhelper, defs_serialqueue, defs_std, defs_stepcompress,
     defs_itersolve, defs_trapq, defs_trdispatch,
     defs_kin_cartesian, defs_kin_corexy, defs_kin_corexz, defs_kin_delta,
-    defs_kin_deltesian, defs_kin_polar, defs_kin_rotary_delta, defs_kin_winch,
+    defs_kin_deltesian, defs_kin_polar, defs_kin_rotary_delta, defs_kin_winch, defs_kin_fivebarelbow,
     defs_kin_extruder, defs_kin_shaper, defs_kin_idex,
 ]
 
